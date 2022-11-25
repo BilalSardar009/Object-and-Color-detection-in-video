3fbf9d43697ed3897f25a674fe7e4a0af6b1c4c1
Bilal Sardar <BilalSardar@users.noreply.huggingface.co>
2022-11-25 16:01:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import cv2
 import gradio as gr
-from google.colab.patches import cv2_imshow
 thres = 0.45 # Threshold to detect object
 
 
@@ -12,7 +11,7 @@ def Detection(filename):
   cap.set(4,720)
   cap.set(10,70)
 
-  error="NoneType' object has no attribute"
+  error="in function 'cv::imshow'"
   classNames= []
   FinalItems=[]
   classFile = 'coco.names'
@@ -35,33 +34,32 @@ def Detection(filename):
   net.setInputSwapRB(True)
 
   while True:
-      success,img = cap.read()
-      try:
-          classIds, confs, bbox = net.detect(img,confThreshold=thres)
-      except:
-          pass
-      print(classIds,bbox)
-      try:
-        if len(classIds) != 0:
-            for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+    success,img = cap.read()
+    try:
+        classIds, confs, bbox = net.detect(img,confThreshold=thres)
+    except:
+        pass
+    print(classIds,bbox)
+    try:
+      if len(classIds) != 0:
+          for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+    
+              cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+              cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+              cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+              cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+              cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+              if FinalItems.count(classNames[classId-1]) == 0:
+                FinalItems.append(classNames[classId-1])
+            
       
-                #cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-                #cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
-                #cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                #cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-                #cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                if FinalItems.count(classNames[classId-1]) == 0:
-                  FinalItems.append(classNames[classId-1])
-              
-        
-        cv2_imshow(img)
-        cv2.waitKey(10)
-      except  Exception as err:
-        print(err)
-        t=str(err)
-        if t.__contains__(error):
-          break
-      
+      cv2.imshow("Output",img)
+      cv2.waitKey(10)
+    except  Exception as err:
+      print(err)
+      t=str(err)
+      if t.__contains__(error):
+        break
 
   print(FinalItems)
   return str(FinalItems)
